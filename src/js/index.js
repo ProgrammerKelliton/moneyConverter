9ec04523d298d40f4a012db51f8f836960ad91b0
Kelliton <programmerKelliton@gmail.com>
2022-06-27 08:24:36 -0300

    Adicionando arquivos

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+const coinSelect = document.getElementById("coin");
+const nameCoin = document.querySelector(".viewValue__currentMoneyValue");
+const sigCoin = document.querySelector(".changeAndView__moneyValue");
+
+coinSelect.addEventListener("change", ChangeText);
+
+function ChangeText() {
+    nameCoin.innerHTML = coinSelect.value + " hoje: R$";
+    sigCoin.innerHTML =
+        coinSelect[coinSelect.selectedIndex].textContent + " 0,00";
+}
