993bdfa3f2f9eb8a6e8279cdc760b2ed3b3223bc
Kelliton <programmerKelliton@gmail.com>
2022-06-27 11:58:13 -0300

    Adicionando selecionador de links

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,49 @@
-const url = "https://economia.awesomeapi.com.br/last/USD-BRL";
-
 const coinSelect = document.getElementById("coin");
 const nameCoin = document.querySelector(".viewValue__currentMoneyValue");
 const sigCoin = document.querySelector(".changeAndView__moneyValue");
 
-coinSelect.addEventListener("change", ChangeText);
+coinSelect.addEventListener("change", API);
 
-function ChangeText() {
-    nameCoin.innerHTML = coinSelect.value + " hoje: R$";
-    sigCoin.innerHTML =
-        coinSelect[coinSelect.selectedIndex].textContent + " 0,00";
+const urls = {
+    Dolar: "https://economia.awesomeapi.com.br/last/USD-BRL",
+    Real: "https://economia.awesomeapi.com.br/last/USD-BRL",
+    Euro: "https://economia.awesomeapi.com.br/last/EUR-BRL",
+};
+
+function SelectUrl(name) {
+    try {
+        for (let count in urls) {
+            if (count == name) {
+                return urls[count];
+            }
+        }
+    } catch (e) {
+        console.log("URL não encontrada, error: " + e);
+    }
+}
+async function API() {
+    let valueMoney = 0;
+    let url = SelectUrl(coinSelect.value);
+
+    let nameMoney = url;
+
+    console.log(nameMoney);
+
+    try {
+        return await fetch(url).then((resp) =>
+            resp
+                .json()
+                .then((data) => data.nameMoney)
+                .then((value) => (valueMoney = value.ask))
+        );
+    } catch (e) {
+        console.log(e);
+    } finally {
+        ChangeText(valueMoney);
+    }
+}
+
+function ChangeText(value) {
+    // Value today
+    nameCoin.innerHTML = coinSelect.value + " hoje: R$ " + value;
 }
